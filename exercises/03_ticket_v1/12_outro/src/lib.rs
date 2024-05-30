725137433f06f4babca65a895608e22c41f54de2
why-not-try-calmer <mrnycticorax@gmail.com>
2024-05-31 00:43:55 +0200

    ---

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,58 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        let mut order = Order {
+            product_name: String::from(""),
+            quantity: 0,
+            unit_price: 0,
+        };
+        order.set_product_name(product_name);
+        order.set_quantity(quantity);
+        order.set_unit_price(unit_price);
+        order
+
+    }
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        if product_name.trim().is_empty() {
+            panic!("Name cannot be empty")
+        }
+        if product_name.len() > 300 {
+            panic!("Name cannot be longer than 300 bytes")
+        }
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        if (quantity) <= 0 {
+            panic!("Quantity cannot be zero or below")
+        }
+        self.quantity = quantity;
+    }
+    pub fn set_unit_price(&mut self, price: u32) {
+        if (price) <= 0 {
+            panic!("Unit price cannot be zero or below")
+        }
+        self.unit_price = price;
+    }
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+}
